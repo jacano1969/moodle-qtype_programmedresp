0805a83d857bb3e46e3c9c477a92c832ef9ae433
David Monllaó <david.monllao@gmail.com>
2012-01-17 11:00:32 +0100

    Solving installation problems

--- version.php
+++ version.php
@@ -1,12 +1,12 @@
 <?PHP
 
-$plugin->version  = 2011102000;
+$plugin->version  = 2012011700;
 $plugin->requires = 2007101509;
-$plugin->release = '1.1 (Build: 2011102000)';
+$plugin->release = '1.2 (Build: 2012011700)';
 
 // To avoid 1.9 Notice
 if (!defined('MATURITY_STABLE')) {
-	define('MATURITY_STABLE',   200);
+	define('MATURITY_STABLE',   200)
 }
 
 // To avoid the M&P warning (yes, sad but true http://www.youtube.com/watch?v=l8BRbM52gpc)
